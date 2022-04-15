ed62979d516126ca661c457d4b777be161805b1e
Michael Nguyen <michaeelnguyen@gmail.com>
2022-04-14 22:47:32 -0700

    Updated Shopping Cart Summary

--- basket/basket.py
+++ basket/basket.py
@@ -1,4 +1,5 @@
-
+from decimal import Decimal
+from storeapp.models import Product
 
 
 class Basket():
@@ -22,9 +23,31 @@ class Basket():
 
 
 
+    def __iter__(self):
+        """
+        Collect the product_id in the session data to query the database and return products
+        """
+        product_ids = self.basket.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        basket = self.basket.copy()
+
+        for product in products:
+            basket[str(product.id)]['product'] = product
+
+        for item in basket.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item
+
+
+
     def __len__(self):
         '''
         Get the basket data and count the item quantities
         '''
         return sum(item['qty'] for item in self.basket.values())
+
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
         
